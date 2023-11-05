2129c59c340e44aaeffdbfccea2f004face4d7b6
Jieun, Jung <126591306+puddingForever@users.noreply.github.com>
2023-11-05 20:10:28 +0900

    Time: 44 ms (41.96%), Space: 43.5 MB (76.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,16 @@
-class Solution {
-    public static void main(String[] args){
+class Solution{
         
-        int[] nums = {3,2,4};
-        int target = 6;
-        
-        twoSum(nums,target);  // 1,6 6-4 = 2 
-        
-    }
-    public static int[] twoSum(int[] nums, int target) {
-        
-        
-        for(int i=0; i<nums.length; i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[j] == target-nums[i]){
-                    return new int[]{i,j};
+         public int[] twoSum(int[] nums,int target){
+
+            for(int i=0;i<nums.length;i++){
+                for(int j=i+1;j<nums.length;j++){
+                    if(nums[j] == target-nums[i]){
+                        return new int[]{i,j}; 
+                    }   
                 }
-                
             }
+
+            return null;
+
         }
-        
-        return null;
-        
-    }
 }
